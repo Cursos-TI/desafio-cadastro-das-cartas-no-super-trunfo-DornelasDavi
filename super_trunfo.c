32d6b91f882dde4666ef5fcbeb8528505b0bf81b
DornelasDavi <dornelas1108@gmail.com>
2025-03-03 13:32:29 +0000

    teste 2

---
+++ super_trunfo.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+int main() {
+
+    char estado; 
+    char codigodacarta[50];
+    char nomedacidade[50];
+    int populacao, pontosturisticos;
+    float area, pib;
+
+
+
+
+
+
+printf("davi\n");
+
+// Carta 1:
+// Estado: A
+// Código: A01
+// Nome da Cidade: São Paulo
+// População: 12325000
+// Área: 1521.11 km²
+//PIB: 699.28 bilhões de reais
+// Número de Pontos Turísticos: 50
+
+
+
+    return 0;
+}
